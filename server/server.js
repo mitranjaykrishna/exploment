4860ecdade853849c225f691f63ef3682b85f884
Mitranjay Krishna <mitranjaykrishna@gmail.com>
2023-05-25 05:49:49 +0530

    second

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const multer = require("multer");
 const fs = require("fs");
+const PORT=process.env.PORT||8000
 
 const app = express();
 const upload = multer({ dest: "uploads/" });
 const { uploadFile } = require("./s3");
 const { detectLabel } = require("./aws");
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static('../client/build'))
+}
 
 app.post("/upload", upload.single("file"), async (req, res) => {
   const file = req.file;
@@ -21,4 +25,4 @@ app.post("/upload", upload.single("file"), async (req, res) => {
 //   res.status(200).send(result.Key);
 });
 
-app.listen(8000, () => console.log("liten on port 8000"));
+app.listen(PORT, () => console.log(`liSten on port ${PORT}`));
